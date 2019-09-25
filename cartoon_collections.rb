cb606822f5dd3f9beabf5e455b526ed6a8d172b1
Rines.io <tsrines@gmail.com>
2019-09-25 16:29:36 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. + #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = %w[earth wind fire water heart]
+
+def summon_captain_planet(planeteer_calls) 
+  planeteer_calls.map { |planet| planet.capitalize + '!'}
+end
+summon_captain_planet(planeteer_calls) 
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |words| words.length > 4 }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+long_planeteer_calls(planeteer_calls)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+stuff = %w[beer latex gouda children]
+
+def find_the_cheese(stuff)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  stuff.find do |have_cheese|
+    cheese_types.include?(have_cheese)
+
+  
+  end
 end
+find_the_cheese(stuff)
+
+
